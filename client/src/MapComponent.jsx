559df1cb3f7cebb97d64601cdb86b2cb8e8507f8
Kyla <kflmorcillos@gmail.com>
2025-06-25 02:34:47 +0800

    Implement map click event

--- client/src/MapComponent.jsx
+++ client/src/MapComponent.jsx
@@ -2,17 +2,22 @@
 
 import { useState } from "react";
 import {
+    AdvancedMarker,
     APIProvider,
-    Map,
-    // AdvancedMarker,
-    // Pin,
-    // InfoWindow
+    Map
 } from "@vis.gl/react-google-maps";
 
 function MapComponent() {
-    const position = {
+    const [clickedLocation, setClickedLocation] = useState(null);
+    const defaultLocation = {
         lat: 14.5875,
         lng: 121.0447
+    }
+
+    function handleMapClick(event) {
+        const lat = event.detail.latLng.lat;
+        const lng = event.detail.latLng.lng;
+        setClickedLocation({ lat, lng });
     }
 
     return <>
@@ -20,9 +25,13 @@ function MapComponent() {
             <div className="h-full w-full">
                 <Map
                     defaultZoom={10}
-                    defaultCenter={position}
+                    defaultCenter={defaultLocation}
                     mapId={import.meta.env.VITE_GOOGLE_MAPS_MAP_ID}
+                    onClick={handleMapClick}
                 >
+                    {clickedLocation && (
+                        <AdvancedMarker position={clickedLocation} />
+                    )}
                 </Map>
             </div>
         </APIProvider>
