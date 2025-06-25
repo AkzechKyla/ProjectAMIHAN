c7643b19aeea9609c9dd217ef0715ad36f75d60c
Kyla <kflmorcillos@gmail.com>
2025-06-25 20:14:07 +0800

    Move handleMapClick outside MapComponent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,43 @@
+import { useState } from "react"
 import MapComponent from './components/MapComponent'
 import SidePanel from './components/SidePanel'
+import Location from './models/Location';
 
 function App() {
-//   const fetchAPI = async () => {
-//     const response = await axios.get("http://127.0.0.1:8080/api/users");
-//     console.log(response.data);
-//   }
-//
-//   useEffect(() => {
-//     fetchAPI();
-//   }, []);
+    const [clickedLocation, setClickedLocation] = useState(null);
+    const defaultLocation = {
+        lat: 14.5875,
+        lng: 121.0447
+    }
 
-  return (
-    <div className="h-screen w-screen flex">
-      <SidePanel />
-      <MapComponent />
-    </div>
-  )
+    async function handleMapClick(event) {
+        const lat = event.detail.latLng.lat;
+        const lng = event.detail.latLng.lng;
+        setClickedLocation({ lat, lng });
+
+        const loc = new Location(lat, lng);
+
+        try {
+            const elevation = await loc.getElevation();
+            const precipitation = await loc.getPrecipitation();
+
+            console.log("Elevation:", elevation);
+            console.log("Precipitation:", precipitation);
+        } catch (error) {
+            console.error("API call failed: ", error);
+        }
+    }
+
+    return (
+      <div className="h-screen w-screen flex">
+        <SidePanel />
+        <MapComponent
+          clickedLocation={clickedLocation}
+          defaultLocation={defaultLocation}
+          handleMapClick={handleMapClick}
+        />
+      </div>
+    )
 }
 
 export default App
--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -1,38 +1,12 @@
 "use client";
 
-import { useState } from "react"
 import {
     AdvancedMarker,
     APIProvider,
     Map
 } from "@vis.gl/react-google-maps"
-import Location from '../models/Location';
 
-function MapComponent() {
-    const [clickedLocation, setClickedLocation] = useState(null);
-    const defaultLocation = {
-        lat: 14.5875,
-        lng: 121.0447
-    }
-
-    async function handleMapClick(event) {
-        const lat = event.detail.latLng.lat;
-        const lng = event.detail.latLng.lng;
-        setClickedLocation({ lat, lng });
-
-        const loc = new Location(lat, lng);
-
-        try {
-            const elevation = await loc.getElevation();
-            const precipitation = await loc.getPrecipitation();
-
-            console.log("Elevation:", elevation);
-            console.log("Precipitation:", precipitation);
-        } catch (error) {
-            console.error("API call failed: ", error);
-        }
-    }
-
+function MapComponent({ clickedLocation, defaultLocation, handleMapClick }) {
     return <>
         <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
             <div className="h-full w-full">
