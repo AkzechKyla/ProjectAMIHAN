f689dcbb6191c3370b48a68d5c12845f7d925f4b
Kyla <kflmorcillos@gmail.com>
2025-06-25 11:49:54 +0800

    Switch to Open-Elevation API

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
@@ -9,24 +9,19 @@ load_dotenv()
 api_key = os.getenv("GOOGLE_MAPS_API_KEY")
 
 app = Flask(__name__)
-cors = CORS(app, origins="*")
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/api/elevation", methods=["GET"])
 def get_elevation():
-    # lat = request.args.get("lat")
-    # lng = request.args.get("lng")
-    lat = 14.5875
-    lng = 121.0447
+    lat = request.args.get("lat")
+    lng = request.args.get("lng")
 
-    url = f"https://maps.googleapis.com/maps/api/elevation/json?locations={lat},{lng}&key={api_key}"
+    url = f"https://api.open-elevation.com/api/v1/lookup?locations={lat},{lng}"
 
     try:
         response = requests.get(url)
         data = response.json()
-
-        if data["status"] == "OK":
-            return jsonify({"elevation": data["results"][0]["elevation"]})
-
+        return jsonify({"elevation": data["results"][0]["elevation"]})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
