79576c9dbfacad03426063880e977c46778f6c63
Kyla <kflmorcillos@gmail.com>
2025-06-26 23:55:13 +0800

    UI: Add prediction results

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -43,46 +43,77 @@ function SidePanel({ location }) {
             <div className="pl-7 z-0">
                 <img src={Logo} alt="Project AMIHAN" />
             </div>
-            <div className="pl-7 pr-15 rounded-xl">
-                <form className="space-y-4 p-6 rounded-lg shadow-[0_0_10px_rgba(0,0,0,0.1)] z-10 relative">
-                    <div className="text-xl text-[--primary-blue] font-semibold">Input Parameters</div>
-                    <InputBox
-                        label="Latitude (°N)"
-                        type="number"
-                        value={formData.latitude}
-                        name="latitude"
-                        setFormData={setFormData}
-                    />
-                    <InputBox
-                        label="Longitude (°E)"
-                        type="number"
-                        value={formData.longitude}
-                        name="longitude"
-                        setFormData={setFormData}
-                    />
-                    <InputBox
-                        label="Elevation (meters)"
-                        type="number"
-                        value={formData.elevation}
-                        name="elevation"
-                        setFormData={setFormData}
-                        smallText="Very Low (0-10), Low (5-20), Medium (15-40), High (30-100)"
-                    />
-                    <InputBox
-                        label="Precipitation (mm)"
-                        type="number"
-                        value={formData.precipitation}
-                        name="precipitation"
-                        setFormData={setFormData}
-                        smallText="Light (μ=5), Moderate (μ=15), Heavy (μ=30), Extreme (μ=50)"
-                    />
-                    <button
-                        type="submit"
-                        className="w-full mt-2 py-2 px-2 rounded-md font-semibold text-sm text-white cursor-pointer"
-                    >
-                        Predict Flood Level
-                    </button>
-                </form>
+            <div className="space-y-8 pb-10">
+                <div className="pl-7 pr-15 rounded-xl">
+                    <form className="space-y-4 p-6 rounded-lg shadow-[0_0_10px_rgba(0,0,0,0.1)] z-10 relative">
+                        <div className="text-xl text-[--primary-blue] font-semibold">Input Parameters</div>
+                        <InputBox
+                            label="Latitude (°N)"
+                            type="number"
+                            value={formData.latitude}
+                            name="latitude"
+                            setFormData={setFormData}
+                        />
+                        <InputBox
+                            label="Longitude (°E)"
+                            type="number"
+                            value={formData.longitude}
+                            name="longitude"
+                            setFormData={setFormData}
+                        />
+                        <InputBox
+                            label="Elevation (meters)"
+                            type="number"
+                            value={formData.elevation}
+                            name="elevation"
+                            setFormData={setFormData}
+                            smallText="Very Low (0-10), Low (5-20), Medium (15-40), High (30-100)"
+                        />
+                        <InputBox
+                            label="Precipitation (mm)"
+                            type="number"
+                            value={formData.precipitation}
+                            name="precipitation"
+                            setFormData={setFormData}
+                            smallText="Light (μ=5), Moderate (μ=15), Heavy (μ=30), Extreme (μ=50)"
+                        />
+                        <button
+                            type="submit"
+                            className="w-full mt-2 py-2 px-2 rounded-md font-semibold text-sm text-white cursor-pointer"
+                        >
+                            Predict Flood Level
+                        </button>
+                    </form>
+                </div>
+                <div className="pl-7 pr-15 rounded-xl">
+                    <div className="p-6 rounded-lg shadow-[0_0_10px_rgba(0,0,0,0.1)] relative">
+                        <div>Prediction Results</div>
+                        <div>
+                            <p>As of 6:09:02 PM, Wednesday June 25, 2025</p>
+                            <p>Philippine Time (UTC+08:00)</p>
+                        </div>
+                        <div>
+                            <p>3.94</p>
+                            <p>Flood Height (0-5 scale)</p>
+                            <div>DANGER</div>
+                        </div>
+                        <hr className="my-4 border-t-2 border-gray-200" />
+                        <div>
+                            <div>
+                                <p>86.9%</p>
+                                <p>Confidence</p>
+                            </div>
+                            <div>
+                                <p>Low</p>
+                                <p>Elevation</p>
+                            </div>
+                            <div>
+                                <p>Moderate</p>
+                                <p>Precipitation</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
