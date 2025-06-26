c56ebd8022ffabb0dbe004d7d583f3b9518e9953
Kyla <kflmorcillos@gmail.com>
2025-06-26 23:56:01 +0800

    Add tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust if your code is in a different directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
