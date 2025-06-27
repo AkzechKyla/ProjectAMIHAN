3b993f2b75c7b363ed0c903f850e7149c72c8f1b
Kyla <kflmorcillos@gmail.com>
2025-06-27 17:52:26 +0800

    Adjust stylings

--- client/src/components/Prediction.jsx
+++ client/src/components/Prediction.jsx
@@ -3,12 +3,14 @@ import warningIcon from '../assets/logo/Triangle_Warning.svg';
 function Prediction({ floodHeight, riskCategory, riskCategoryColor, riskLevel, elevationLevel, precipitationLevel }) {
     return(
         <div className="pl-7 pr-15 rounded-xl">
-            <div className="p-6 rounded-lg shadow-[0_0_10px_rgba(0,0,0,0.1)]">
+            <div className="p-6 rounded-lg shadow-[0_0_10px_rgba(0,0,0,0.1)] min-h-[353px] min-w-[584px]">
+                <div className="text-xl text-[--primary-blue] font-semibold">Prediction Results</div>
                 {floodHeight === null &&
-                    <p>Select a location and click "Predict Flood Level" to see results.</p>
+                    <div className="flex justify-center items-center min-h-[353px]">
+                        <p className="text-sm font-normal">Select a location and click "Predict Flood Level" to see results.</p>
+                    </div>
                 }
                 {floodHeight !== null && <div>
-                    <div className="text-xl text-[--primary-blue] font-semibold">Prediction Results</div>
                     <div className="text-sm text-gray-400 mt-2 mb-4">
                         <p>As of <span className="font-bold">6:09:02 PM, Wednesday June 25, 2025</span></p>
                         <p>Philippine Time (UTC+08:00)</p>
@@ -17,9 +19,9 @@ function Prediction({ floodHeight, riskCategory, riskCategoryColor, riskLevel, e
                         <div className="flex flex-col text-center items-center">
                             <p className="text-3xl font-medium">{floodHeight.toFixed(2)}</p>
                             <p className="text-sm text-gray-400 mt-2">Flood Height (0-5 scale)</p>
-                            <div className="inline-flex w-24 items-center text-center text-white text-sm font-semibold px-4 py-2 rounded-xl mt-4 mb-4" style={{ backgroundColor: riskCategoryColor }}>
-                                <img src={warningIcon} className="pr-1"/>
-                                {riskCategory}
+                            <div className="inline-flex w-24 justify-center items-center gap-1 text-white text-sm font-semibold px-4 py-2 rounded-xl mt-4 mb-4" style={{ backgroundColor: riskCategoryColor }}>
+                                <img src={warningIcon} />
+                                <span>{riskCategory}</span>
                             </div>
                         </div>
                     </div>
